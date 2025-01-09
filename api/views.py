4afbec9c56b68c03120893a899dbf18a915a853d
Biringanine Destin <biringaninedestin@gmail.com>
2025-01-09 13:47:33 +0200

    trying to add viewset on url

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import *
+from .serializers import *
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
