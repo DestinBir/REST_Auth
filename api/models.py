9fcfe1b919ac1f49d44738235b7cb514d5425ffb
Biringanine Destin <biringaninedestin@gmail.com>
2025-01-09 13:37:27 +0200

    creating the product model

--- api/models.py
+++ api/models.py
@@ -9,3 +9,14 @@ class User(AbstractUser):
     zip_code = models.CharField(max_length=10, blank=True, null=True)
     country = models.CharField(max_length=100, blank=True, null=True)
     profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
